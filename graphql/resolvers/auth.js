116a8483b51c8f2bef8fc872d7d43fdf46b765ba
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-14 22:10:36 -0500

    Refactored code by moving all mutations related to users to a new file

---
+++ graphql/resolvers/auth.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcrypt");
+const User = require("../../models/User");
+
+module.exports = {
+  createUser: async args => {
+    try {
+      const existingUser = await User.findOne({ email: args.userInput.email });
+      if (existingUser) {
+        throw new Error("User already exists");
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword
+      });
+      const result = await user.save();
+      return { ...result._doc, password: null };
+    } catch (error) {
+      throw error;
+    }
+  }
+};
