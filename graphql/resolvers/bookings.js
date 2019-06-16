26e9cc8fd1a3c2bb146db4c76c04f2c04d4fb427
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-15 19:31:48 -0500

    Protecting queries and mutations that need an user to be logged in

--- graphql/resolvers/bookings.js
+++ graphql/resolvers/bookings.js
@@ -2,7 +2,10 @@ const Booking = require("../../models/booking");
 const Event = require("../../models/event");
 const { transformEvent, transformBooking } = require("./merge");
 module.exports = {
-  bookings: async () => {
+  bookings: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
@@ -13,7 +16,10 @@ module.exports = {
     }
   },
 
-  bookEvent: async args => {
+  bookEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
     const fetchedEvent = await Event.findOne({ _id: args.eventId });
     const booking = new Booking({
       user: "5cfdc8b9f157bb25f419dc85",
@@ -23,7 +29,10 @@ module.exports = {
     return transformBooking(result);
   },
 
-  cancelBooking: async args => {
+  cancelBooking: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
     try {
       const booking = await Booking.findById(args.bookingId).populate("event");
       const event = transformEvent(booking.event);
