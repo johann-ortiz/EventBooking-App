d74e0ffed9c7b5ff5f5ee6f82fcd690e66c22423
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-17 22:25:13 -0500

    Added email and password states to the Auth component, implemented handleChange method for the values of the inputs and submitHandler to aims to the createUser mutation

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -2,16 +2,64 @@ import React, { Component } from "react";
 import "./Auth.css";
 
 export default class Auth extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+
+    const requestBody = {
+      query: `
+      mutation {
+        createUser(userInput: {email: "${this.state.email}", password: "${
+        this.state.password
+      }"}) {
+        _id
+        email
+      }
+      }
+      `
+    };
+
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      body: JSON.stringify(),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  };
+
   render() {
     return (
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={this.submitHandler}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={this.handleChange}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={this.handleChange}
+          />
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
