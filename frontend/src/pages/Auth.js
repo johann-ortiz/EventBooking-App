a3044f6545be45d405d0e91577fbb53ed6b91828
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-17 22:03:37 -0500

    Added form to the Auth to enter email and password, and added its respective CSS

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+import "./Auth.css";
 
 export default class Auth extends Component {
   render() {
-    return <div>AuthPage</div>;
+    return (
+      <form className="auth-form">
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to SignUp</button>
+        </div>
+      </form>
+    );
   }
 }
