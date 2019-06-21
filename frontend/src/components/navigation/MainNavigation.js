31b52717fd491359676df4a10e12b1c0e32ed0c2
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-20 22:09:46 -0500

    Added conditional rendering for each link depending if the link requires a token or not

--- frontend/src/components/navigation/MainNavigation.js
+++ frontend/src/components/navigation/MainNavigation.js
@@ -1,26 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./MainNavigation.css";
+import AuthContext from "../../context/auth-context";
 
 export default function Main() {
   return (
-    <header className="main-navigation">
-      <div className="main-navigation-logo">
-        <h1>Event App</h1>
-      </div>
-      <nav className="main-navigation-items">
-        <ul>
-          <li>
-            <NavLink to="/auth">Authenticate</NavLink>
-          </li>
-          <li>
-            <NavLink to="/events">Events</NavLink>
-          </li>
-          <li>
-            <NavLink to="/bookings">Bookings</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <AuthContext.Consumer>
+      {context => {
+        return (
+          <header className="main-navigation">
+            <div className="main-navigation-logo">
+              <h1>Event App</h1>
+            </div>
+            <nav className="main-navigation-items">
+              <ul>
+                {!context.token && (
+                  <li>
+                    <NavLink to="/auth">Authenticate</NavLink>
+                  </li>
+                )}
+                <li>
+                  <NavLink to="/events">Events</NavLink>
+                </li>
+                {context.token && (
+                  <li>
+                    <NavLink to="/bookings">Bookings</NavLink>
+                  </li>
+                )}
+              </ul>
+            </nav>
+          </header>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 }
