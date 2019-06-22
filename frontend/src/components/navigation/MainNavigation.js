e5ded7e88f0a2571bc02ba85b6e5dfc21d6b78de
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-21 23:06:49 -0500

    Changed CSS styles for the MainNavigation colors

--- frontend/src/components/navigation/MainNavigation.js
+++ frontend/src/components/navigation/MainNavigation.js
@@ -23,9 +23,14 @@ export default function Main() {
                   <NavLink to="/events">Events</NavLink>
                 </li>
                 {context.token && (
-                  <li>
-                    <NavLink to="/bookings">Bookings</NavLink>
-                  </li>
+                  <React.Fragment>
+                    <li>
+                      <NavLink to="/bookings">Bookings</NavLink>
+                    </li>
+                    <li>
+                      <button>Logout</button>
+                    </li>
+                  </React.Fragment>
                 )}
               </ul>
             </nav>
