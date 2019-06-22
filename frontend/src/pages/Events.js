1ef3b99123bb5781b106107fb26b8454c6c5b82a
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-21 23:22:50 -0500

    Added button to create an event in Events Page, added its respective CSS style and moved the buttons style from Auth.css to a general file called Index.css because it will be used over more components

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import "./Events.css";
 
 export default class Events extends Component {
   render() {
-    return <div>Events</div>;
+    return (
+      <div className="events-control">
+        <p>Share Your Events</p>
+        <button className="btn">Create Event</button>
+      </div>
+    );
   }
 }
