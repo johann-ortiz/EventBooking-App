7c9eb846eabf7cc071a58eceaef143789fc705d0
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-22 23:06:54 -0500

    Implemented handlers for cancel modal and confirm modal and passed them as props to the Modal component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -12,12 +12,26 @@ export default class Events extends Component {
     this.setState({ creating: true });
   };
 
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
     return (
       <React.Fragment>
         {this.state.creating && <Backdrop />}
         {this.state.creating && (
-          <Modal title="Add Event" canCancel canConfirm>
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
             <p>Modal Content</p>
           </Modal>
         )}
