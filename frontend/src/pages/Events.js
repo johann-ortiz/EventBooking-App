a33ad6ddf1ceec946be1df5b49d5f25c259c9f4e
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-22 23:02:37 -0500

    Imported backdrop - Created state to conditional render the backdrop and the modal - Implemented handler for create event button

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import "./Events.css";
 import Modal from "../components/modal/Modal";
+import Backdrop from "../components/backdrop/Backdrop";
 
 export default class Events extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Modal title="Add Event" canCancel canConfirm>
-          <p>Modal Content</p>
-        </Modal>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal title="Add Event" canCancel canConfirm>
+            <p>Modal Content</p>
+          </Modal>
+        )}
         <div className="events-control">
           <p>Share Your Events</p>
-          <button className="btn">Create Event</button>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
         </div>
       </React.Fragment>
     );
