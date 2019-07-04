3e876f973e1f81dee595f75614bae263cd8fad4b
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-07-03 21:43:57 -0500

    Implemented usersLoader for a faster query

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -7,9 +7,13 @@ const eventLoader = new DataLoader(eventIds => {
   return events(eventIds);
 });
 
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
+
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
